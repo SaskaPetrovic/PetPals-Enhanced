63ae2e9982813ab50c5164b1eb49a6f7870df700
saska petrovic <saskapetrovic.pro@gmail.com>
2024-07-10 15:13:58 +0200

    index

--- app/controllers/animals_controller.rb
+++ app/controllers/animals_controller.rb
@@ -1,10 +1,17 @@
 class AnimalsController < ApplicationController
 
   def index
-    @animals = Animal.all
+    @animal = Animal.all
+    @cats = Animal.where(especes: 'Cat')
+    @dogs = Animal.where(especes: 'Dog')
+    @rodents = Animal.where(especes: 'Rodent')
+    @reptiles = Animal.where(especes: 'Reptile')
+    @fishes = Animal.where(especes: 'Fish')
+    @birds = Animal.where(especes: 'Bird')
   end
 
   def show
+    @animal = Animal.find(params[:id])
   end
 
   def new
