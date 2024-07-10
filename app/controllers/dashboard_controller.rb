47bb0409c6c9f76e49bbcfcf8d1064fa64f30611
Alan Quevreux <alan.quevreux@gmail.com>
2024-07-10 11:44:34 +0200

    access_dashboard

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,5 +1,8 @@
 class DashboardController < ApplicationController
+  before_action :authenticate_user!
 
   def index
+    @reservations = current_user.reservations
+    @animals = current_user.animals
   end
 end
