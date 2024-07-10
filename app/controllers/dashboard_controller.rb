5ab75efa3959808cac51fc71a736bf38a152c1b0
Alan Quevreux <alan.quevreux@gmail.com>
2024-07-10 16:41:59 +0200

    nav_link et resa

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -2,7 +2,7 @@ class DashboardController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    @reservations = current_user.reservations
-    @animals = current_user.animals
+    @reservations = current_user.reservations.includes(:animal)
+    @animals = current_user.animals.limit(3)
   end
 end
--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -1,15 +1,25 @@
 class ReservationsController < ApplicationController
-
-  def index
-  end
+  before_action :authenticate_user!
+  before_action :set_animal
 
   def create
+    @reservation = @animal.reservations.new(reservation_params)
+    @reservation.user = current_user
+
+    if @reservation.save
+      redirect_to dashboard_path, notice: 'Réservation créée avec succès.'
+    else
+      redirect_to animal_path(@animal), alert: 'Erreur lors de la création de la réservation.'
+    end
   end
 
-  def update
+  private
+
+  def set_animal
+    @animal = Animal.find(params[:animal_id])
   end
 
-  def show
+  def reservation_params
+    params.require(:reservation).permit(:date_debut, :date_fin)
   end
-
 end
